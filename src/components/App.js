e06ac17b38e3de6a7f7999a377197eeb39eb2199
Artem Boyur <gboyur@gmail.com>
2017-02-11 14:16:22 +0300

    HW3.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,20 @@
 import React, { PropTypes, Component } from 'react'
 import ArticleList from './ArticleList'
-import Chart from './Chart'
-import Select from 'react-select'
-import 'react-select/dist/react-select.css'
 
 class App extends Component {
-    state = {
-        user: '',
-        selection: null
-    }
 
     render() {
-        const {articles} = this.props
-        const options = articles.map(article => ({
-            label: article.title,
-            value: article.id
-        }))
+        const {articles} = this.props;
         return (
             <div>
-                User: <input type="text" value={this.state.user} onChange={this.handleUserChange}/>
-                <Select options = {options} onChange={this.handleSelectChange} value={this.state.selection} multi/>
-                <ArticleList articles={articles}/>
-                <Chart articles={articles}/>
+                <ArticleList articles={articles} isOpenItem='' toggleOpenItem=''/>
             </div>
         )
-    }
-
-    handleSelectChange = selection => this.setState({ selection })
-
-    handleUserChange = (ev) => {
-        if (ev.target.value.length < 10) {
-            this.setState({
-                user: ev.target.value
-            })
-        }
     }
 }
 
 App.propTypes = {
     articles: PropTypes.array.isRequired
-}
+};
 
 export default App
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,55 +1,75 @@
 import React, { Component, PropTypes } from 'react'
+import { Button, Form, Input, Textarea } from 'muicss/react';
 import Comment from './Comment'
+import 'muicss/dist/css/mui.css'
 
 class CommentList extends Component {
     static propTypes = {
         comments: PropTypes.array
-    }
+    };
     static defaultProps = {
         comments: []
-    }
-    componentDidMount() {
-        console.log('---', 'mounted')
-    }
-
-    componentWillReceiveProps(nextProps) {
-       // console.log('---', this.props, nextProps)
-    }
-
-
-    componentWillUnmount() {
-        //console.log('---', 'unmounting')
-    }
+    };
+    // componentDidMount() {
+    //     console.log('---', 'mounted')
+    // }
+    //
+    // componentWillReceiveProps(nextProps) {
+    //    // console.log('---', this.props, nextProps)
+    // }
+    //
+    //
+    // componentWillUnmount() {
+    //     //console.log('---', 'unmounting')
+    // }
 
     state = {
         isOpen: false
-    }
+    };
 
     render() {
-        const actionText = this.state.isOpen ? 'hide' : 'show'
+        const actionText = this.state.isOpen ? 'hide' : 'show';
         return (
             <div>
                 <a href="#" onClick={this.toggleOpen}>{actionText} comments</a>
                 {this.getBody()}
+                {this.addComment()}
             </div>
         )
     }
 
     getBody() {
-        if (!this.state.isOpen) return null
+        if (!this.state.isOpen) return null;
 
-        const {comments} = this.props
-        if (!comments.length) return <h3>No comments yet</h3>
+        const {comments} = this.props;
+        if (!comments.length) return <h3>No comments yet</h3>;
 
-        const commentItems = comments.map(comment => <li key={comment.id}><Comment comment={comment} /></li>)
+        const commentItems = comments.map(comment => <li key={comment.id}><Comment comment={comment} /></li>);
+
         return <ul>{commentItems}</ul>
     }
 
+    addComment() {
+        if (!this.state.isOpen) return null;
+
+        return <Form onSubmit={this.handleSubmit}>
+                  <Input type="text" name="user" hint="User"/>
+                  <Textarea name="comment" hint="Text..."/>
+                  <Button type="submit" color="primary">Add comment</Button>
+               </Form>;
+    }
+
     toggleOpen = ev => {
-        ev.preventDefault()
+        ev.preventDefault();
         this.setState({
             isOpen: !this.state.isOpen
         })
+    };
+
+    handleSubmit = (ev) => {
+        alert('User: ' + ev.target.user.value +
+              '\nComment: ' + ev.target.comment.value);
+        ev.preventDefault();
     }
 }
 
